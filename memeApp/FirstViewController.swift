9e78f998dfaedafa1b435f1cec5c21670e9c6a78
Masters3d <cheyo@masters3d.com>
2015-05-13 00:33:46 +0545

    Init

---
+++ memeApp/FirstViewController.swift
@@ -0,0 +1,56 @@
+//
+//  FirstViewController.swift
+//  memeApp
+//
+//  Created by masters3d on 5/12/15.
+//  Copyright (c) 2015 masters3d. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet var cameraButtonOutlet: UIButton!
+    
+
+    func showCameraPicker(){
+        println("need camera picker")
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.navigationController?.navigationBarHidden = true
+
+        
+        let imageForSmallCameraButton = RBSquareImageTo(cameraButtonOutlet.imageView!.image!, CGSize(width: 20, height: 20))
+        
+        
+//        let cameraButton = UIBarButtonItem(image:imageForSmallCameraButton , style: UIBarButtonItemStyle.Plain, target: self, action: "showCameraPicker")
+       let cameraButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Camera, target: self, action: "showCameraPicker")
+        
+
+
+        //self.navigationItem.leftBarButtonItem = cameraButton
+        
+        self.setToolbarItems([cameraButton], animated: false)
+                // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
